c02b0b4c80442316f6e714bee3c82846d3aa2c94
orangedays <juife@foxmail.com>
2018-11-18 23:43:47 +0800

    add two numbers

---
+++ 17-add-two-numbers.js
@@ -0,0 +1,33 @@
+// https://leetcode-cn.com/problems/add-two-numbers/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+  let carry = 0, result, head;
+  while (l1 || l2) {
+      const sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+      carry = sum > 9 ? 1 : 0;
+      const curr = new ListNode(sum % 10)
+      if (!result) {
+          result = head = curr;
+      } else {
+          head.next = curr;
+          head = curr;
+      }
+      l1 = l1 ? l1.next : null;
+      l2 = l2 ? l2.next : null;
+  }
+  if (carry) {
+      head.next = new ListNode(carry);
+  }
+  return result;
+};
