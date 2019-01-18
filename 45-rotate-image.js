b41465f0f313685397bb919edf1a69bca9e38612
orangedays <juife@foxmail.com>
2019-01-18 17:04:45 +0800

    add: 48.rotate image

---
+++ 45-rotate-image.js
@@ -0,0 +1,24 @@
+// https://leetcode-cn.com/problems/rotate-image
+/**
+ * time: O(n * m)
+ * space: O(1)
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < i; j++) {
+      const temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < Math.floor(matrix[0].length / 2); j++) {
+      const temp = matrix[i][j];
+      matrix[i][j] = matrix[i][matrix.length - 1 - j];
+      matrix[i][matrix.length - 1 - j] = temp;
+    }
+  }
+  return matrix;
+};
